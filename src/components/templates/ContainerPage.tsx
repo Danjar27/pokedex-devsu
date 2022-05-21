a0a13ed5f0d9df560d2469bfa6ea042d71d1a309
Danjar27 <losteven2018@outlook.com>
2022-05-21 17:24:37 -0500

    created a container page

--- src/components/templates/ContainerPage.tsx
+++ src/components/templates/ContainerPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const ContainerPage:React.FC<any> = ({children}) => {
+const ContainerPage: React.FC<any> = ({children}) => {
   return (
-    <div className="container">
-      {children}
-    </div>
+    <>
+      <div className="container_background"/>
+      <div className="container">
+        {children}
+      </div>
+    </>
   );
 }
 
